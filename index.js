1cd431edfaf934bb447f03100299e9c41b0308af
mougali <sarthakk92@gmail.com>
2016-06-18 17:57:12 -0500

    Removed front-page response

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
 
 // Server frontpage
-app.get('/', function (req, res) {
-    res.send('This is TestBot Server');
-});
+// app.get('/', function (req, res) {
+//     res.send('1729018094');
+// });
 
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
